1822b05e952a158f658d4960616595fd23df1c3e
Tyler LaRocca <tylerlarocca@Tylers-MacBook-Pro.local>
2023-11-09 20:51:20 -0800

    Corrections

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { listDecks, deleteDeck } from "./utils/api/index";
 import CreateDeck from "./CreateDeck";
+import "./Home.css"
 
 
 function Home() {
@@ -34,16 +35,17 @@ function Home() {
   return (
     <div>
       <h2>Decks</h2>
-      <Link to="./CreateDeck">Create Deck</Link>
-
-      <ul>
+      <Link to="/decks/new" className="Create btn btn-secondary">Create Deck</Link>
+      
+      <ul id="DeckBox">
         {decks.map((deck) => (
-          <li key={deck.id}>
-            <p>{deck.name}</p>
-            <p>{deck.cards.length} cards</p>
-            <Link to={`/study/${deck.id}`}>Study</Link>
-            <Link to={`/view/${deck.id}`}>View</Link>
-            <button onClick={() => handleDelete(deck.id)}>Delete</button>
+          <li key={deck.id} class="List">
+            <p id="CardNum">{deck.cards.length} cards</p>
+            <p id="DeckName">{deck.name}</p>
+            <p>{deck.description}</p>
+            <Link to={`/decks/${deck.id}`} className="btn btn-secondary">View</Link>
+            <Link to={`/decks/${deck.id}/study`} className="Study btn btn-primary">Study</Link>
+            <button onClick={() => handleDelete(deck.id)} className="Delete btn btn-danger">Delete</button>
           </li>
         ))}
       </ul>
